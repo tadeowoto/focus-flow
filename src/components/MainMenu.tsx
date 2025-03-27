79e291cffeff086040cca243665416bfb74937ef
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-03-27 13:19:01 -0300

    menu logic

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -4,19 +4,41 @@ export const MainMenu = () => {
   //TODO IF !ISFOCUSACTIVE: MAIN CONTENT SHOULD BE AMBIENT SOUNDS
 
   const [isFocusActive, setIsFocusActive] = useState(false);
+  const [isRelaxActive, setIsRelaxActive] = useState(true);
+  const handleFocusMenu = () => {
+    setIsFocusActive(true);
+    setIsRelaxActive(false);
+  };
+
+  const handleRelaxMenu = () => {
+    setIsRelaxActive(true);
+    setIsFocusActive(false);
+  };
 
   return (
     <article className="w-full h-fit flex flex-col gap-8">
       <div className=" flex items-center  justify-center h-10 w-full ">
         <nav className="w-14/15 h-full flex rounded-md bg-cloud ">
           <div className="flex w-full h-full items-center justify-around">
-            <button>Relaxation</button>
-            <button>Focus</button>
+            <button onClick={handleRelaxMenu}>Relaxation</button>
+            <button onClick={handleFocusMenu}>Focus</button>
           </div>
         </nav>
       </div>
       <div className="w-full bg-amber-200 h-200">
-        {isFocusActive ? <p>Focus</p> : <p>Relax</p>}
+        {isFocusActive ? (
+          <div>
+            <h1>
+              <p>Se mostraria el focus</p>
+            </h1>
+          </div>
+        ) : isRelaxActive ? (
+          <div>
+            <h1>
+              <p>Se mostraria el ambient </p>
+            </h1>
+          </div>
+        ) : null}
       </div>
     </article>
   );
