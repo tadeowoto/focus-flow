6c5fbc929d82ae08fec50002014fe75955940197
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-03-31 15:13:30 -0300

    pomodoro logic added

--- src/components/FocusPomodoro.tsx
+++ src/components/FocusPomodoro.tsx
@@ -1,36 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const FocusPomodoro = () => {
-  const [selectedTime, setSelectedTime] = useState(25);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(25 * 60);
+  const [isBreak, setIsBreak] = useState(false);
+  const [audio] = useState(new Audio("/bell.wav"));
+  useEffect(() => {
+    let timer: number | null = null;
 
-  const handleTimeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedTime(parseInt(event.target.value));
+    if (isPlaying && selectedTime > 0) {
+      timer = setInterval(() => {
+        setSelectedTime((prevTime) => {
+          if (prevTime <= 1) {
+            audio.play();
+            if (isBreak) {
+              setIsBreak(false);
+              setSelectedTime(25 * 60); //pomodoro de nuevo
+              setIsPlaying(false); //detener el pomodoro
+              return 25 * 60;
+            } else {
+              setIsBreak(true);
+              setSelectedTime(5 * 60); //descanso de nuevo
+              setIsPlaying(false); //detener el pomodoro
+              return 5 * 60;
+            }
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    }
+
+    // Limpiar el interval cuando se pausa o el componente se desmonta
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [isPlaying, selectedTime]);
+
+  const handlePomodoro = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
   return (
-    <>
-      <h1 className="text-2xl font-semibold text-slate">Pomodoro</h1>
-      <div className="w-full h-full flex flex-col items-center justify-center gap-10">
-        <select
-          value={selectedTime}
-          onChange={handleTimeChange}
-          className="h-10 p-2 rounded-lg border text-slate-700 border-seafoam-secondary bg-cloud"
-        >
-          <option value={25}>25 minutes</option>
-          <option value={30}>30 minutes</option>
-          <option value={50}>50 minutes</option>
-          <option value={60}>60 minutes</option>
-        </select>
-        <p className="text-6xl font-semibold text-slate">{selectedTime}:00</p>
-      </div>
-      <div className="w-full flex items-center justify-evenly">
-        <button className="w-30 h-10 rounded-lg text-cloud bg-seafoam-secondary">
-          Play
-        </button>
-        <button className="w-30 h-10 rounded-lg text-slate bg-cloud border border-seafoam-secondary">
-          Restart
-        </button>
-      </div>
-    </>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-semibold text-slate">Pomodoro Timer</h1>
+      <p className="text-md font-bold text-lavender">
+        {isBreak ? "chill and serv a mate" : "Time on, lets focus"}
+      </p>
+      <div className="text-4xl text-slate my-4">{formatTime(selectedTime)}</div>
+      <button
+        className={`w-30 h-10 rounded-lg ${
+          isPlaying
+            ? "text-slate bg-cloud border border-seafoam-secondary"
+            : "text-cloud bg-seafoam-secondary"
+        }`}
+        onClick={handlePomodoro}
+      >
+        {isPlaying ? "Pause" : "Start"}
+      </button>
+    </div>
   );
 };
